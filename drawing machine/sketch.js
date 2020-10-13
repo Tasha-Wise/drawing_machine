684ca788f8b8e122f710a3a35f865fb656556b96
Tasha-Wise <60452066+Tasha-Wise@users.noreply.github.com>
2020-10-13 16:29:46 -0700

    button press

--- drawing machine/sketch.js
+++ drawing machine/sketch.js
@@ -1,7 +1,16 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
+  background(220, 50, 133);
+  srokeWeight(5);
 }
 
+
 function draw() {
-  background(220);
+	if (mouseIsPressed){
+		stroke(map(mouseX, 0, 600, 0, 255, true))
+		line(width - mouseX, height - mouseY, width - pmouseX, Height - pmouseY);
+		line(mouseX, mouseY, pmouseX, pmouseY);
+
+	}
+  
 }
