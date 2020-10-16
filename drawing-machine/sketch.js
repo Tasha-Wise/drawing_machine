c4bb54c13cc2edc1e2b5416e4cb644f96aee57ad
Tasha-Wise <60452066+Tasha-Wise@users.noreply.github.com>
2020-10-15 18:29:48 -0700

    push

--- drawing-machine/sketch.js
+++ drawing-machine/sketch.js
@@ -1,14 +1,13 @@
 let array = [];
-let button1;
-let button2;
 let slider;
 let r = 100;
 let g = 100;
 let b = 100;
+let backgroundColor = (220, 50, 133);
 
 function setup() {
-  createCanvas(600, 600);
-  background(220, 50, 133);
+  createCanvas(windowWidth, windowWHeight);
+  background(backgroundColor);
   strokeWeight(5);
   noFill();
 
@@ -49,11 +48,20 @@ function draw() {
 
 	if (mouseIsPressed){
 		stroke(map(mouseX, 0, 600, 0, 255, true))
-		background(0),
+		background(backgroundColor);
+		backgroundColor -= 5;
 		line(pmouseX, pmouseY, mouseX, mouseY)
-		//line(width - mouseX, height - mouseY, width - pmouseX, Height - pmouseY);
+		line(width - mouseX, height - mouseY, width - pmouseX, Height - pmouseY);
 		//line(mouseX, mouseY, pmouseX, pmouseY);
 		array.push([mouseX, mouseY])
+
+		beginShape();
+		for(let i = 0; i < array.length - 1; i++ ){
+			console.log(i);
+			//line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1],);
+			curveVertex (array[i][0], array[i][1]);
+		}
+endShape();
 	}
   
 }
@@ -82,5 +90,10 @@ endShape();
 		
 	}
 
+return false;
+}
+function mousePressed(){
+array = [];	
+backgroundColor = (30, 255, 40);
 
 }
